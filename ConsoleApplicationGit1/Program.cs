dbdf923a5957e61735e56969c4d1f1026b6c26c6
Andriy <bismarck195@gmail.com>
2016-09-24 13:49:10 +0300

    First test

---
+++ ConsoleApplicationGit1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationGit1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            int c=a+b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
